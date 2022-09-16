9031d7f6356fcb73d1eea08761876e0665089624
Ben Brill <benbrill101@gmail.com>
2022-09-16 14:48:24 -0700

    Add Guide Pages

---
+++ src/pages/contact.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Seo from '../components/seo'
+import Menu from '../components/navbar'
+import Layout from '../components/layout'
+import { Form } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
+
+const Contact = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e);
+  }
+  return (
+    <Seo title = "Contact">
+    <Layout> 
+    <Form onSubmit={ handleSubmit } id = "myForm">
+      <Form.Group className="mb-3" controlId="name">
+        <Form.Label>Name</Form.Label>
+        <Form.Control type="text" placeholder="name" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="email">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+        
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+
+    </Layout>
+    </Seo>
+
+  )
+}
+
+export default Contact
