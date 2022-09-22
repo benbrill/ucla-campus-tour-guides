5db311f4c54466412df372457dacc800a76ab184
Ben Brill <benbrill101@gmail.com>
2022-09-21 20:07:25 -0700

    Add disclaimer

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
-import Layout from "../components/layout"
 import Seo from "../components/seo"
 import * as styles from "../components/index.module.css"
 import Countdown from "../components/countdown"
@@ -20,6 +19,14 @@ const IndexPage = () => (
   <>
     <Seo title="Home" />
     <Menu />
+    <div style = {{backgroundColor: "#2774AE", width: "100%", minHeight: "30px", color: "#ffffff", textAlign: "center",
+      display: "flex"}}>
+      <div style = {{margin: "auto auto", padding: "10px 0"}}> 
+      Applicants worried about the summer requirment are encouraged to apply regardless of summer availability.
+       See the <Link to = "\faq" style = {{color : "#ffffff"}}>FAQ</Link> page for more details
+      </div>
+    
+      </div>
     <HeroLanding />
     <div style={{display: "flex", alignItems:"center", justifyContent: "center", flexDirection: "column"}}>
       {/* hero */}
@@ -28,6 +35,14 @@ const IndexPage = () => (
       
     </div>
     <Countdown />
+    <div style = {{backgroundColor: "#ff5d00", width: "100%", minHeight: "30px", color: "#ffffff", textAlign: "center",
+      display: "flex", marginTop: "20px", flexDirection: "column", display: "none"}}>
+      {/* <StaticImage src="../images/duffl.png" alt="" width = {3} aspectRatio = {1}/> */}
+      <div style = {{margin: "auto auto", padding: "10px 0"}}> 
+      New <Link to = "https://www.duffl.com/?view=Landing" style = {{color: "#ffffff"}} target = "_blank">Duffl</Link> users can use the code "uclacampustours" for $5 off their first order! 
+      </div>
+    
+      </div>
     <Explainer />
     <Testimonial />
     <Footer />
