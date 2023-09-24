8ceae077e41f5fd79d436f56689e6bbcbbe40631
Ben Brill <benbrill101@gmail.com>
2023-09-24 16:10:00 -0700

    Alter triangle mobile

--- src/pages/rip.jsx
+++ src/pages/rip.jsx
@@ -16,11 +16,7 @@ const rip = () => {
             <StaticImage src="../images/guide_photos/jyang.jpg" aspectRatio={1} transformOptions={{cropFocus: "center"}}/>
         </div>
         <p style={{maxWidth: "100%", textAlign: "center", margin: "20px 0 0 0"}}>
-            This is a commemoration to the guides who were unjustly terminated by UCLA Undergraduate Admissions, on the day of their graduation.
-            These guides were loyal, compentent, and passionate members of the program. They were the victims of an unnecessary vendeta carried out by supervisor Casey Shubin to weed out fun from the program.
-            They were "terminated" after they had completed their final tour and no longer paid employees of the program, demonstrating the vindictive nature of the termination. The reason for their termination? 
-            They had shared a toast of a bottle champagne presented by each of their parents on their final tour. This was a common practice among guides, and was never an issue until Casey Shubin decided to make it one.
-            These were the guides singled out. Do not forget the injustices committed against them. 
+        This is a commemoration to the guides who were unjustly terminated by UCLA Undergraduate Admissions, on the day of their graduation. These guides were loyal, competent, and passionate members of the program. They were the victims of an unnecessary vendetta carried out by supervisor Casey Shubin to weed out fun from the program. They were "terminated" after they had completed their final tour and no longer paid employees of the program, demonstrating the vindictive nature of the termination. The reason for their termination? They had shared a toast of Champagne presented by each of their parents on their final tour. This was a common practice among guides and was never an issue until Casey Shubin decided to make it one. These were the guides singled out. Do not forget the injustices committed against them.
         </p>
     </Layout>
   )
